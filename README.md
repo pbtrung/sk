12a7606442c25b0c7be687f4b99e1bd607cb6bbe
Trung Pham <phambaotrung@gmail.com>
2019-01-23 09:09:56 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sk ![Alt License](https://dl.dropboxusercontent.com/s/cul64jahsd3cg14/license.svg?dl=0)
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#define ZPL_IMPLEMENTATION
+#include "zpl.h"
+
+int main(int argc, char **argv) { 
+	return EXIT_SUCCESS; 
+}
