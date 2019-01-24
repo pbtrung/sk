01167649fa2cec9cd4deaad08625003de4042be3
Trung Pham <phambaotrung@gmail.com>
2019-01-24 01:12:00 -0800

    Rewrite

--- src/main.c
+++ src/main.c
@@ -5,11 +5,128 @@
 
 #include "argparse/argparse.h"
 #include "argon2/argon2.h"
+#include "sqlite3/sqlite3.h"
 
-int main(int argc, char **argv) {
+#define PWD_LEN 256
+#define SALT_LEN 32
+#define PWD_FILE_SIZE (PWD_LEN + SALT_LEN)
+
+unsigned char *sk_sodium_malloc(size_t s) {
+    unsigned char *buf = sodium_malloc(s);
+    if (buf == NULL) {
+        stb_fatal("cannot allocate memory.");
+    }
+    return buf;
+}
+
+void sk_zero_free(unsigned char *buf, size_t buf_len) {
+    sodium_memzero(buf, buf_len);
+    sodium_free(buf);
+}
+
+FILE *sk_fopen(const char *path, const char *flags) {
+    FILE *f = stb__fopen(path, flags);
+    if (f == NULL) {
+        stb_fatal("cannot open file.");
+    }
+    return f;
+}
+
+unsigned char *sk_create_local_db(size_t *db_buf_size) {
+    sqlite3 *db = NULL;
+    char *err_msg = NULL;
+    
+    int rc = sqlite3_open(":memory:", &db);
+    if (rc != SQLITE_OK) {
+        stb_fatal("cannot create a new database.");
+    }
+    char *sql = "DROP TABLE IF EXISTS keys;"
+                "CREATE TABLE keys(version INT, hc256_iv BLOB, ciphertext BLOB, hmac BLOB);"
+                "DROP TABLE IF EXISTS secrets;"
+                "CREATE TABLE secrets(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, username TEXT, url TEXT, salt BLOB, notes TEXT, length INT, special INT);"
+                "DROP TABLE IF EXISTS tags;"
+                "CREATE TABLE tags(secret_id INT, name TEXT, notes TEXT);";
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    if (rc != SQLITE_OK) {
+        stb_fatal("SQL error: %s\n", err_msg);
+    }
+    unsigned char *db_buf = sqlite3_serialize(db, "main", db_buf_size, 0);
+    if (db_buf == NULL) {
+        stb_fatal("cannot serialize database.");
+    }
+    sqlite3_close(db);
+    return db_buf;
+}
+
+void sk_create_new_db(const char *pwd_file_path, const char *path, int path_type) {
+    FILE *pwd_file = sk_fopen(pwd_file_path, "rb");
+
+    size_t file_size = stb_filelen(pwd_file);
+    if (file_size != PWD_FILE_SIZE) {
+        stb_fatal("password file does not have required size (%d bytes).", PWD_FILE_SIZE);
+    }
+
+    unsigned char *fc = sk_sodium_malloc(PWD_FILE_SIZE);
+    size_t len = fread(fc, 1, PWD_FILE_SIZE, pwd_file);
+    if (len != PWD_FILE_SIZE) {
+        stb_fatal("cannot read file.");
+    }
+    fclose(pwd_file);
+
+    unsigned char *pwd = sk_sodium_malloc(PWD_LEN);
+    unsigned char *salt = sk_sodium_malloc(SALT_LEN);
+    
+    memcpy(pwd, fc, PWD_LEN);
+    memcpy(salt, &fc[PWD_LEN], SALT_LEN);
+    sk_zero_free(fc, PWD_FILE_SIZE);
+
+    if (path_type == 1) {
+        size_t db_buf_size = 0;
+        unsigned char *db_buf = sk_create_local_db(&db_buf_size);
+        // sk_encrypt(pwd, salt, path, db_buf, db_buf_size);
+        sqlite3_free(db_buf);
+    }
+
+    sk_zero_free(pwd, PWD_LEN);
+    sk_zero_free(salt, SALT_LEN);
+}
+
+static const char *const usage[] = {
+    "sk [options] [[--] args]",
+    "sk [options]",
+    NULL,
+};
+
+int main(int argc, const char **argv) {
 
     if (sodium_init() < 0) {
         stb_fatal("libsodium cannot be initialized.");
+    }
+
+    const char *pwd_file_path = NULL;
+    const char *local_path = NULL;
+    const char *url = NULL;
+    int new = 0;
+    struct argparse_option options[] = {
+        OPT_HELP(),
+        OPT_BOOLEAN('n', "new", &new, "create new database"),
+        OPT_STRING('p', "pwd-file", &pwd_file_path, "path to password file (REQUIRED)"),
+        OPT_STRING('l', "local-path", &local_path, "local path to database"),
+        OPT_STRING('u', "url", &url, "remote path (URL) to database"),
+        OPT_END(),
+    };
+    struct argparse argparse;
+    argparse_init(&argparse, options, usage, 0);
+    argparse_describe(&argparse, "\nSecret Keeper (sk). Options:", "");
+    argparse_parse(&argparse, argc, argv);
+
+    if (new != 0) {
+        if (argc == 6 && pwd_file_path != NULL && (local_path != NULL || url != NULL)) {
+            if (local_path != NULL)
+                sk_create_new_db(pwd_file_path, local_path, 1);
+            if (url != NULL)
+                sk_create_new_db(pwd_file_path, url, 2);
+        }
     }
 
     return EXIT_SUCCESS;
